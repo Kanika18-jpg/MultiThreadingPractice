2ffe1574cfc6b1fefe17ea37f1c9026490e1a9da
hp <sabhrwalkanika1999@gmail.com>
2023-10-13 23:11:16 +0530

    Add project files.

---
+++ MultiThreadingPractice/Bank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreadingPractice
+{
+    internal class Bank
+    {
+        private static readonly object lockObject = new object();
+        private static int balance = 1000;
+        public static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ParameterizedThreadStart(WithDraw));
+            Thread t2 = new Thread(new ParameterizedThreadStart(WithDraw));
+
+            t1.Start(300);
+            t2.Start(1100);
+            t1.Join();
+            t2.Join();
+
+            Console.Read();
+        }
+        static void WithDraw(object num)
+        {
+            lock (lockObject)
+            {
+                int withdrawalAmount = (int)num;
+                if(balance >= withdrawalAmount)
+                {
+                    balance -= withdrawalAmount;
+                    Console.WriteLine($"Balance : {balance}");
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient balance...");
+                }
+            }
+
+        }
+    }
+}
---
+++ MultiThreadingPractice/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreadingPractice
+{
+    internal class Program
+    {
+        private static readonly object lockObject = new object();
+        private static int count = 1;
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(PrintNumbers);
+            Thread t2 = new Thread(PrintNumbers);
+
+            t1.Name = "1";
+            t2.Name = "2";
+
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+
+            Console.Read();
+                
+        }
+        static void PrintNumbers()
+        {
+            while (count <= 10)
+            {
+                lock (lockObject) // Lock to prevent interleaving
+                {
+                    if (count <= 10)
+                    {
+                        Console.WriteLine($"Thread {Thread.CurrentThread.Name}: {count}");
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
